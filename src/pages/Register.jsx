70068bbd06a45d88d7cd12944d58bc4f6b40775f
Zamahsyari <zmachmobile@gmail.com>
2020-01-02 23:17:14 +0700

    added select DOB

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,56 @@ import "./Login.scss";
 import { Link } from "react-router-dom";
 
 class Register extends React.Component {
+  getDays() {
+    let days = [];
+    for (let i = 1; i <= 31; i++) {
+      days.push(i);
+    }
+    return days.map(item => {
+      if (item < 10) {
+        return <option value={"0" + item}>{"0" + item}</option>;
+      } else {
+        return <option value={item}>{item}</option>;
+      }
+    });
+  }
+  getMonths() {
+    let months = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember"
+    ];
+    let monthIndexes = [];
+    for (let i = 1; i <= 12; i++) {
+      monthIndexes.push(i);
+    }
+    return months.map((item, i) => {
+      if (i < 10) {
+        return <option value={"0" + (i + 1)}>{item}</option>;
+      } else {
+        return <option value={i + 1}>{item}</option>;
+      }
+    });
+  }
+  getYears() {
+    let years = [];
+    let now = new Date();
+    for (let i = 1900; i <= now.getFullYear() - 18; i++) {
+      years.push(i);
+    }
+    return years.map(item => {
+      return <option value={item}>{item}</option>;
+    });
+  }
   render() {
     return (
       <Template>
@@ -27,16 +77,19 @@ class Register extends React.Component {
                   <Col>
                     <Form.Control as="select">
                       <option>Tanggal</option>
+                      {this.getDays()}
                     </Form.Control>
                   </Col>
                   <Col>
                     <Form.Control as="select">
                       <option>Bulan</option>
+                      {this.getMonths()}
                     </Form.Control>
                   </Col>
                   <Col>
                     <Form.Control as="select">
                       <option>Tahun</option>
+                      {this.getYears()}
                     </Form.Control>
                   </Col>
                 </Form.Row>
