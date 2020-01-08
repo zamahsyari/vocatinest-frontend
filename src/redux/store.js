f88f959e77eff48974c6bd410d64f6a5b71bbb9f
Zamahsyari <zmachmobile@gmail.com>
2020-01-09 04:19:09 +0700

    disable redux plugin

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,11 +11,11 @@ const store = createStore(
     searchResults: searchResultsReducer,
     profile: profileReducer
   }),
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-  // applyMiddleware(thunk)
+  // compose(
+  //   applyMiddleware(thunk),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
+  applyMiddleware(thunk)
 );
 
 export default store;
